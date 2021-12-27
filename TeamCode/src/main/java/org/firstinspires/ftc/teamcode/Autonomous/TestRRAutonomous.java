6df6c59f064f4d6ece33bd6560b76a62749fec34
Keval Shah <kevals2005@gmail.com>
2021-12-26 21:46:56 -0500

    Adjusted the TestRRAutonomous program to fit our onlyCyclesBlue program
    Includes a configurable number of cycles to do and a state machine so that we can easily add intake/lift code later
    
    Added comments as to where to put intake/lift code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestRRAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestRRAutonomous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -28,161 +29,110 @@ import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
  * to supercharge your code. This can be much cleaner by abstracting many of these things. This
  * opmode only serves as an initial starting point.
  */
+@Config
 @Autonomous(group = "advanced")
 public class TestRRAutonomous extends LinearOpMode {
 
     // This enum defines our "state"
     // This is essentially just defines the possible steps our program will take
     enum State {
-        GO_TO_HUB_1,   // First, follow a splineTo() trajectory
-        TRAJECTORY_2,   // Then, follow a lineTo() trajectory
-        TURN_1,         // Then we want to do a point turn
-        TRAJECTORY_3,   // Then, we follow another lineTo() trajectory
-        WAIT_1,         // Then we're gonna wait a second
-        TURN_2,         // Finally, we're gonna turn again
-        IDLE            // Our bot will enter the IDLE state when done
+        PLACE_PRELOADED_BLOCK,
+        CYCLE,
+        DELIVER_CARGO,
+        PARK_IN_WAREHOUSE,
+        IDLE
     }
 
-    // We define the current state we're on
-    // Default to IDLE
-    State currentState = State.IDLE;
+    public static int numCycles = 4;
+    public static double waitTime = 0.5;
+    private int currentCycle = 0;
+
+    State currentState = State.PLACE_PRELOADED_BLOCK;
 
     // Define our start pose
     // This assumes we start at x: 15, y: 10, heading: 180 degrees
-    Pose2d startPose = new Pose2d(15, 10, Math.toRadians(180));
+    Pose2d startPose = new Pose2d(12, 63, Math.toRadians(-90));
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize our lift
         Lift lift = new Lift(hardwareMap);
-
-        // Initialize SampleMecanumDrive
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        // Set inital pose
+        // Set the initial pose of the robot
         drive.setPoseEstimate(startPose);
 
-        // Let's define our trajectories
-        Trajectory trajectory1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(45, -20), Math.toRadians(90))
+        Trajectory placePreloadedBlock = drive.trajectoryBuilder(startPose)
+                .lineToLinearHeading(new Pose2d(0, 38, Math.toRadians(-120)))
                 .build();
 
-        // Second trajectory
-        // Ensure that we call trajectory1.end() as the start for this one
-        Trajectory trajectory2 = drive.trajectoryBuilder(trajectory1.end())
-                .lineTo(new Vector2d(45, 0))
+        Trajectory cycle = drive.trajectoryBuilder(placePreloadedBlock.end(), true)
+                .splineToSplineHeading(new Pose2d(30, 65.5, Math.toRadians(180)), Math.toRadians(0))
+                .lineTo(new Vector2d(42, 65.5))
+                .lineTo(new Vector2d(30, 65.5))
+                .splineTo(new Vector2d(0, 38), Math.toRadians(-120))
                 .build();
 
-        // Define the angle to turn at
-        double turnAngle1 = Math.toRadians(-270);
-
-        // Third trajectory
-        // We have to define a new end pose because we can't just call trajectory2.end()
-        // Since there was a point turn before that
-        // So we just take the pose from trajectory2.end(), add the previous turn angle to it
-        Pose2d newLastPose = trajectory2.end().plus(new Pose2d(0, 0, turnAngle1));
-        Trajectory trajectory3 = drive.trajectoryBuilder(newLastPose)
-                .lineToConstantHeading(new Vector2d(-15, 0))
+        Trajectory parkInWarehouse = drive.trajectoryBuilder(cycle.end(), true)
+                .lineToLinearHeading(new Pose2d(12, 65.5, Math.toRadians(180)))
+                .lineToConstantHeading(new Vector2d(42, 65.5))
                 .build();
 
-        // Define a 1.5 second wait time
-        double waitTime1 = 1.5;
-        ElapsedTime waitTimer1 = new ElapsedTime();
-
-        // Define the angle for turn 2
-        double turnAngle2 = Math.toRadians(720);
+        ElapsedTime waitTimer = new ElapsedTime();
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        // Set the current state to TRAJECTORY_1, our first step
-        // Then have it follow that trajectory
-        // Make sure you use the async version of the commands
-        // Otherwise it will be blocking and pause the program here until the trajectory finishes
-        currentState = State.GO_TO_HUB_1;
-        drive.followTrajectoryAsync(trajectory1);
-        // Extend lift
+        drive.followTrajectoryAsync(placePreloadedBlock);
+        // put lift to correct height
 
         while (opModeIsActive() && !isStopRequested()) {
-            // Our state machine logic
-            // You can have multiple switch statements running together for multiple state machines
-            // in parallel. This is the basic idea for subsystems and commands.
-
-            // We essentially define the flow of the state machine through this switch statement
             switch (currentState) {
-                case GO_TO_HUB_1:
-                    // Check if the drive class isn't busy
-                    // `isBusy() == true` while it's following the trajectory
-                    // Once `isBusy() == false`, the trajectory follower signals that it is finished
-                    // We move on to the next state
-                    // Make sure we use the async follow function
+                case PLACE_PRELOADED_BLOCK:
+                    // put lift to correct height
                     if (!drive.isBusy()) {
-                        currentState = State.TRAJECTORY_2;
-                        drive.followTrajectoryAsync(trajectory2);
+                        currentState = State.DELIVER_CARGO;
+                        waitTimer.reset();
                     }
                     break;
-                case TRAJECTORY_2:
-                    // Check if the drive class is busy following the trajectory
-                    // Move on to the next state, TURN_1, once finished
-                    if (!drive.isBusy()) {
-                        currentState = State.TURN_1;
-                        drive.turnAsync(turnAngle1);
-                    }
-                    break;
-                case TURN_1:
-                    // Check if the drive class is busy turning
-                    // If not, move onto the next state, TRAJECTORY_3, once finished
-                    if (!drive.isBusy()) {
-                        currentState = State.TRAJECTORY_3;
-                        drive.followTrajectoryAsync(trajectory3);
-                    }
-                    break;
-                case TRAJECTORY_3:
-                    // Check if the drive class is busy following the trajectory
-                    // If not, move onto the next state, WAIT_1
-                    if (!drive.isBusy()) {
-                        currentState = State.WAIT_1;
 
-                        // Start the wait timer once we switch to the next state
-                        // This is so we can track how long we've been in the WAIT_1 state
-                        waitTimer1.reset();
+                case CYCLE:
+                    // set intake power to 1
+                    // if there's a block inside the basket, reverse intake
+                    if (!drive.isBusy()) {
+                        currentState = State.DELIVER_CARGO;
                     }
                     break;
-                case WAIT_1:
-                    // Check if the timer has exceeded the specified wait time
-                    // If so, move on to the TURN_2 state
-                    if (waitTimer1.seconds() >= waitTime1) {
-                        currentState = State.TURN_2;
-                        drive.turnAsync(turnAngle2);
+
+                case DELIVER_CARGO:
+                    if (waitTimer.seconds() > waitTime) {
+                        currentCycle++;
+                        // set the basket servo to the collection position
+                        if (currentCycle <= numCycles) {
+                            currentState = State.CYCLE;
+                            drive.followTrajectoryAsync(cycle);
+                        } else {
+                            currentState = State.PARK_IN_WAREHOUSE;
+                            drive.followTrajectoryAsync(parkInWarehouse);
+                        }
                     }
                     break;
-                case TURN_2:
-                    // Check if the drive class is busy turning
-                    // If not, move onto the next state, IDLE
-                    // We are done with the program
+
+                case PARK_IN_WAREHOUSE:
                     if (!drive.isBusy()) {
                         currentState = State.IDLE;
                     }
                     break;
+
                 case IDLE:
-                    // Do nothing in IDLE
-                    // currentState does not change once in IDLE
-                    // This concludes the autonomous program
                     break;
             }
 
-            // Anything outside of the switch statement will run independent of the currentState
-
-            // We update drive continuously in the background, regardless of state
             drive.update();
-            // We update our lift PID continuously in the background, regardless of state
             lift.update();
 
-            // Read pose
             Pose2d poseEstimate = drive.getPoseEstimate();
 
-            // Print pose to telemetry
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
