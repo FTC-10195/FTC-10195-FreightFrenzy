f60d63a4c0ad52369d02680baeb1feafd175ee53
Keval Shah <kevals2005@gmail.com>
2022-02-07 11:58:03 -0500

    Added new controls for intake controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumTeleOp.java
@@ -25,14 +25,15 @@ public class MecanumTeleOp extends LinearOpMode {
 
     public void mainLoop(Robot robot) {
         robot.drive(
+                telemetry,
                 gamepad1.left_stick_x,
                 gamepad1.left_stick_y,
                 gamepad1.right_stick_x,
                 gamepad1.left_trigger,
                 gamepad1.right_bumper,
                 gamepad1.left_bumper,
-                gamepad1.a,
-                gamepad1.b,
+                gamepad2.right_trigger > 0.1,
+                gamepad2.left_trigger > 0.1,
                 gamepad1.dpad_up || gamepad2.x,
                 gamepad1.dpad_down || gamepad2.b,
                 gamepad2.dpad_up,
