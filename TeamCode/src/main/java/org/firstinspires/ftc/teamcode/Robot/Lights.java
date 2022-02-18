b80a47ceafcbcd66d634d6db4c2f4696ecd2828d
Keval Shah <kevals2005@gmail.com>
2022-02-18 08:45:28 -0500

    Fixed bugs in Lights class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Lights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Lights.java
@@ -5,29 +5,30 @@ import static com.qualcomm.hardware.rev.RevBlinkinLedDriver.BlinkinPattern;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Lights extends Subsystem {
     RevBlinkinLedDriver lights;
-    private BlinkinPattern pattern;
 
     public final BlinkinPattern FREIGHT_DETECTED = BlinkinPattern.RAINBOW_RAINBOW_PALETTE;
-    public final BlinkinPattern STANDARD_PATTERN = BlinkinPattern.CP1_2_END_TO_END_BLEND;
+    public final BlinkinPattern STANDARD_PATTERN = BlinkinPattern.BLUE_VIOLET;
+
+    private BlinkinPattern pattern = STANDARD_PATTERN;
 
     FreightDetector freightDetector;
+
+    static Telemetry telemetry;
 
     public Lights(HardwareMap hwMap) {
         lights = hwMap.get(RevBlinkinLedDriver.class, "lights");
         freightDetector = new FreightDetector(hwMap, dashboard);
     }
 
-    public void setPattern(BlinkinPattern pattern) {
-        this.pattern = pattern;
-    }
-
-    public void freightDetected() {
+    public void checkBasket() {
         if (freightDetector.freightDetected()) {
-            lights.setPattern(FREIGHT_DETECTED);
+            pattern = FREIGHT_DETECTED;
         } else {
-            lights.setPattern(STANDARD_PATTERN);
+            pattern = STANDARD_PATTERN;
         }
     }
 
